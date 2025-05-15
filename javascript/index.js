d8096f0b6014b0fd66b65f4148a99efd47985c0f
jessjat <mutanujat@gmail.com>
2025-05-15 12:09:33 +0300

    Added the current location

--- javascript/index.js
+++ javascript/index.js
@@ -24,6 +24,9 @@ losAngelesTimeElement.innerHTML = losAngelesTime.format ("h:mm:ss:ss [<small>]A[
 
 function updateCity(event){
     let cityTimeZone = event.target.value;
+     if(cityTimeZone==="current"){
+       cityTimeZone=moment.tz.guess(); 
+     }
     let cityTime=moment().tz(cityTimeZone);
     let cityName=cityTimeZone.replace("_"," ").split("/")[1];
     let citiesElement=document.querySelector("#cities");
